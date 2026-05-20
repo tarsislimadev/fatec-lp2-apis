e223104bbcfc7a297461a0d348b5b7fee6f0d326
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-05-20 20:42:45 +0000

    Fix formatting of base URL string in config module

--- src/client/config.js
+++ src/client/config.js
@@ -1,6 +1,6 @@
 const getPathName = () => '/api/v1'
 
-const getBaseURL = () => (window.location == 'localhost:5000' ? `http://localhost:5000` : `https://redesigned-garbanzo-6ww6gqr56vw24v7-5000.app.github.dev`) + getPathName();
+const getBaseURL = () => (window.location == 'localhost:5000' ? 'http://localhost:5000' : 'https://redesigned-garbanzo-6ww6gqr56vw24v7-5000.app.github.dev') + getPathName();
 
 export const config = {
   urls: {
