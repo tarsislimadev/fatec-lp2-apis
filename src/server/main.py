d27531fa10c3f41aead00458c6c9dafa953de05c
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-05-20 10:44:25 -0300

    Refactor API endpoint to retrieve dog breeds and update response structure; add requests library to requirements

--- src/server/main.py
+++ src/server/main.py
@@ -1,13 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
+import requests as req
 
 app = Flask(__name__)
 
+url = 'https://dogapi.dog/api/v2/breeds?page%5Bnumber%5D=1&page%5Bsize%5D=10'
+
+req_headers = {
+  'accept': 'application/json'
+}
+
+res_headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+}
+
 CORS(app)
 
-@app.route('/api/data', methods=['POST'])
+@app.route('/api/v1/dogs', methods=['POST'])
 def receive_data():
-  return request.get_json(), 200
+  dogs = req.get(url, headers=req_headers).json()
+  resp = make_response(jsonify({"message": "Data received successfully", "dogs": dogs}), 200)
+  resp.headers.extend(res_headers)
+  return resp
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
