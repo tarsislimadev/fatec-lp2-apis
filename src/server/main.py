94d253f0e5121fd839a6ed67ebd33c5f4e533ee0
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-05-20 15:29:19 +0000

    Refactor dog API response handling and remove debug prints

--- src/server/main.py
+++ src/server/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, make_response
 from flask_cors import CORS
 import requests as req
 
 app = Flask(__name__)
+CORS(app)
 
 base_url = 'https://dogapi.dog/api/v2'
 
@@ -10,29 +11,25 @@ req_headers = {
   'accept': 'application/json'
 }
 
-resp_headers = {
+res_headers = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': '*',
 }
 
-CORS(app)
-
 @app.route('/api/v1/dogs', methods=['POST'])
 def list_dogs():
-  print("Received request for list of dogs")
   dogs = req.get(f'{base_url}/breeds', headers=req_headers).json()
-  resp = make_response(jsonify({"message": "Data received successfully", "dogs": dogs}), 200)
-  resp.headers = resp.headers.extend(resp_headers)
+  resp = make_response({"dogs": dogs}, 200)
+  resp.headers = resp.headers.extend(res_headers)
   return resp
 
 @app.route('/api/v1/dogs/<id>', methods=['POST'])
 def get_dog(id):
-  print(f"Received request for dog with id: {id}")
   dog = req.get(f'{base_url}/breeds/{id}', headers=req_headers).json()
-  resp = make_response(jsonify({"message": "Dog found", "dog": dog}), 200)
-  resp.headers = resp.headers.extend(resp_headers)
+  resp = make_response({"dog": dog}, 200)
+  resp.headers = resp.headers.extend(res_headers)
   return resp
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run()
